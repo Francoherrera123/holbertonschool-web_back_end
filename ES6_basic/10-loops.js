5664802369122d21eb821ac3e7249e69ed9b84d0
Francoherrera123 <francoh261@gmail.com>
2023-09-17 15:48:08 +0000

    Tasks fixed

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -3,6 +3,6 @@ export default function appendToEachArrayValue(array, appendString) {
     for (const idx of array) {
       newArray.push(appendString + idx);
     }
-  
+
     return newArray;
   }
